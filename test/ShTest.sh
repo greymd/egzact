489cc09aef7f24482b3e31a404b05e261122d7db
greymd <greengregson@gmail.com>
2016-04-24 03:15:33 +0900

    Add usages

--- test/ShTest.sh
+++ test/ShTest.sh
@@ -6,6 +6,115 @@ setUp(){
 	cd $BIN_DIR 2> /dev/null
 }
 
+test_addt() {
+	result=`echo abc | ./addt.egi ABC`
+	assertEquals "ABC
+abc" "${result}"
+
+	result=`echo a bc | ./addt.egi ofs=_ eos=--- A`
+	assertEquals "A
+---
+a_bc" "${result}"
+}
+
+test_addb() {
+	result=`echo abc | ./addb.egi ABC`
+	assertEquals "abc
+ABC" "${result}"
+
+	result=`echo a bc | ./addb.egi ofs=_ eos=--- A`
+	assertEquals "a_bc
+---
+A" "${result}"
+}
+
+test_addl() {
+	result=`echo abc | ./addl.egi ABC`
+	assertEquals "ABCabc" "${result}"
+}
+
+test_addr() {
+	result=`echo abc | ./addr.egi ABC`
+	assertEquals "abcABC" "${result}"
+}
+
+test_comb() {
+	result=`echo A B C D | ./comb.egi 2`
+	assertEquals "A B
+A C
+B C
+A D
+B D
+C D" "${result}"
+
+	result=`echo "A B C\n1 2 3" | ./comb.egi eos="---" 2`
+	assertEquals "A B
+A C
+B C
+---
+1 2
+1 3
+2 3" "${result}"
+}
+
+test_crops() {
+	result=`echo "(* (cos α) (cos β))" | ./crops.egi "\(.*\)"`
+	assertEquals "(* (cos α)
+(cos α)
+(* (cos α) (cos β)
+(cos α) (cos β)
+(cos β)
+(* (cos α) (cos β))
+(cos α) (cos β))
+(cos β))" "${result}"
+}
+
+test_cycle() {
+	result=`echo A B C D E | cycle`
+	assertEquals "A B C D E
+B C D E A
+C D E A B
+D E A B C
+E A B C D" "${result}"
+}
+
+test_dropl() {
+	result=`echo A B C D E F G | ./dropl.egi 3`
+	assertEquals "D E F G" "${result}"
+
+	result=`echo A B C D E F G | ./dropl.egi ofs=_ 3`
+	assertEquals "D_E_F_G" "${result}"
+}
+
+test_dropr() {
+	result=`echo A B C D E F G | ./dropr.egi 3`
+	assertEquals "A B C D" "${result}"
+
+	result=`echo A B C D E F G | ./dropr.egi ofs=_ 3`
+	assertEquals "A_B_C_D" "${result}"
+}
+
+test_dupl() {
+	result=`echo A B C D E F G | ./dupl.egi 3`
+	assertEquals "A B C D E F G
+A B C D E F G
+A B C D E F G" "${result}"
+
+	result=`echo "A B C\nD E F G" | ./dupl.egi ofs=_ eos=--- 3`
+	assertEquals "A_B_C
+A_B_C
+A_B_C
+---
+D_E_F_G
+D_E_F_G
+D_E_F_G" "${result}"
+}
+
+test_mirror() {
+	result=`echo aaa bbb ccc aaa | ./mirror.egi`
+	assertEquals "aaa ccc bbb aaa" "${result}"
+}
+
 test_takel() {
 	result=`echo A B C D E F G | ./takel.egi 3`
 	assertEquals "A B C" "${result}"
@@ -39,10 +148,33 @@ test_takerx() {
 }
 
 test_wrap() {
-	result=`echo aaa bbb ccc | wrap "<p>*</p>"`
+	result=`echo aaa bbb ccc | ./wrap.egi "<p>*</p>"`
     assertEquals "<p>aaa</p> <p>bbb</p> <p>ccc</p>" "${result}"
+
+	result=`echo aaa bbb ccc | ./wrap.egi ofs=_ "<p>*</p>"`
+    assertEquals "<p>aaa</p>_<p>bbb</p>_<p>ccc</p>" "${result}"
 }
 
+test_zniq() {
+	result=`echo aaa bbb ccc aaa bbb | ./zniq.egi`
+    assertEquals "aaa bbb ccc" "${result}"
 
+	result=`echo aaa bbb ccc aaa bbb | ./zniq.egi ofs="*"`
+    assertEquals "aaa*bbb*ccc" "${result}"
+}
+
+test_zrep() {
+	result=`echo 1 2 3 4 5 6 7 8 9 10 | ./zrep.egi "1"`
+    assertEquals "1 10" "${result}"
+
+	result=`echo 1 2 3 4 5 6 7 8 9 10 | ./zrep.egi 1`
+    assertEquals "1 10" "${result}"
+
+	result=`echo AD1 AD2 AF1 AF2 BD1 BD2 BF1 BF2 CD1 CD2 CF1 CF2 DD1 DD2 DF1 DF2 | ./zrep.egi '^D.*$'`
+    assertEquals "DD1 DD2 DF1 DF2" "${result}"
+
+	result=`echo AD1 AD2 AF1 AF2 BD1 BD2 BF1 BF2 CD1 CD2 CF1 CF2 DD1 DD2 DF1 DF2 | ./zrep.egi 'あ'`
+    assertEquals "" "${result}"
+}
 
 . ${TEST_DIR}/shunit2/source/2.1/src/shunit2
