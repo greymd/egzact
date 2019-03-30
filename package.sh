2a2175d1d9badaff8ca3f29e5375dcea7de26bf7
greymd <greengregson@gmail.com>
2019-03-30 11:53:27 +0000

    Update package

--- package.sh
+++ package.sh
@@ -3,6 +3,7 @@ set -ue
 readonly THIS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]:-${(%):-%N}}")" && pwd)"
 {
   cd "${THIS_DIR}"
+  rm -f "${THIS_DIR}"/egzact.tar.gz
   mkdir -p "${THIS_DIR}/pkg/"{bin,lib}
   (
   cd "${THIS_DIR}/bin"
@@ -11,8 +12,10 @@ readonly THIS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]:-${(%):-%N}}")" && pwd)"
   done
   )
   cp -rf "${THIS_DIR}"/lib/egzact "${THIS_DIR}"/pkg/lib/
+  _version="$(awk '/^version/{print $NF}' "${THIS_DIR}"/pkg/.tar2package.yml)"
   tar zcvf egzact.tar.gz -C "${THIS_DIR}/pkg" bin lib .tar2package.yml \
-    && docker run -i greymd/tar2rpm < egzact.tar.gz > "${THIS_DIR}"/pkg/egzact.rpm \
-    && docker run -i greymd/tar2deb < egzact.tar.gz > "${THIS_DIR}"/pkg/egzact.deb
-  rm -f egzact.tar.gz
+    && docker run -i greymd/tar2rpm < egzact.tar.gz > "${THIS_DIR}"/pkg/egzact-"${_version}".rpm \
+    && docker run -i greymd/tar2deb < egzact.tar.gz > "${THIS_DIR}"/pkg/egzact-"${_version}".deb
+  rm -f "${THIS_DIR}"/egzact.tar.gz
+  rm -rf "${THIS_DIR}"/pkg/{bin,lib}
 }
