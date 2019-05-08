82c06a9c28ce7ad1cbc7fc7516c4ce8c19ccc37a
greymd <greengregson@gmail.com>
2019-05-08 22:35:05 +0100

    update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,31 @@
 #!/usr/bin/env bash
-make install
+set -ue
+
+readonly THIS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]:-${(%):-%N}}")" && pwd)"
+readonly BINMODE=755
+readonly DOCMODE=644
+readonly PREFIX="${1:-/usr/local}"
+readonly PREFIX_BIN="${PREFIX}/bin"
+readonly PREFIX_LIB="${PREFIX}/lib/egzact"
+
+# Install (bin)
+echo install -d "${PREFIX_BIN}"
+install -d "${PREFIX_BIN}"
+{
+  cd "${THIS_DIR}/bin/"
+  for _file in * ;do
+    echo install -m "${BINMODE}" "${THIS_DIR}/bin/${_file}" "${PREFIX_BIN}/${_file/.egi}"
+    install -m "${BINMODE}" "${THIS_DIR}/bin/${_file}" "${PREFIX_BIN}/${_file/.egi}"
+  done
+}
+
+# Install (lib)
+echo install -d "${PREFIX_LIB}"
+install -d "${PREFIX_LIB}"
+{
+  cd "${THIS_DIR}/lib/egzact/"
+  for _file in * ;do
+    echo install -m "${BINMODE}" "${THIS_DIR}/lib/${_file}" "${PREFIX_LIB}/${_file/.egi}"
+    install -m "${BINMODE}" "${THIS_DIR}/lib/${_file}" "${PREFIX_LIB}/${_file/.egi}"
+  done
+}
