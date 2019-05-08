9c6b522e1b3de0e6687263e1658b81dfabdbbee2
greymd <greengregson@gmail.com>
2019-05-08 23:21:11 +0100

    update installer

--- install.sh
+++ install.sh
@@ -25,7 +25,7 @@ install -d "${PREFIX_LIB}"
 {
   cd "${THIS_DIR}/lib/egzact"
   for _file in * ;do
-    echo install -m "${LIBMODE}" "${THIS_DIR}/lib/egzact/${_file}" "${PREFIX_LIB}/${_file}"
-    install -m "${LIBMODE}" "${THIS_DIR}/lib/egzact/${_file}" "${PREFIX_LIB}/${_file}"
+    echo sed "2,3s:\\.\\.:${PREFIX}:" "${THIS_DIR}/lib/egzact/${_file}  > ${PREFIX_LIB}/${_file}"
+    sed "2,3s:\\.\\.:${PREFIX}:" "${THIS_DIR}/lib/egzact/${_file}" > "${PREFIX_LIB}/${_file}"
   done
 }
