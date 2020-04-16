ba3e50c6bbe09bf409c4474ec0ddd960fd24016a
theoremoon <theoldmoon0602@gmail.com>
2020-04-16 21:45:59 +0900

    save

--- test/ShTest.sh
+++ test/ShTest.sh
@@ -7,39 +7,39 @@ setUp(){
 }
 
 test_addt() {
-	result=`echo abc | ./addt.egi ABC`
+	result=`echo abc | egison ./addt.egi -- ABC`
 	assertEquals "ABC
 abc" "${result}"
 
-	result=`echo a bc | ./addt.egi ofs=_ eos=--- A`
+	result=`echo a bc | egison ./addt.egi -- ofs=_ eos=--- A`
 	assertEquals "A
 ---
 a_bc" "${result}"
 }
 
 test_addb() {
-	result=`echo abc | ./addb.egi ABC`
+	result=`echo abc | egison ./addb.egi -- ABC`
 	assertEquals "abc
 ABC" "${result}"
 
-	result=`echo a bc | ./addb.egi ofs=_ eos=--- A`
+	result=`echo a bc | egison ./addb.egi -- ofs=_ eos=--- A`
 	assertEquals "a_bc
 ---
 A" "${result}"
 }
 
 test_addl() {
-	result=`echo abc | ./addl.egi ABC`
+	result=`echo abc | egison ./addl.egi -- ABC`
 	assertEquals "ABCabc" "${result}"
 }
 
 test_addr() {
-	result=`echo abc | ./addr.egi ABC`
+	result=`echo abc | egison ./addr.egi -- ABC`
 	assertEquals "abcABC" "${result}"
 }
 
 test_comb() {
-	result=`echo A B C D | ./comb.egi 2`
+	result=`echo A B C D | egison ./comb.egi -- 2`
 	assertEquals "A B
 A C
 B C
@@ -47,7 +47,7 @@ A D
 B D
 C D" "${result}"
 
-	result=`printf "A B C\n1 2 3" | ./comb.egi eos="---" 2`
+	result=`printf "A B C\n1 2 3" | egison ./comb.egi -- eos="---" 2`
 	assertEquals "A B
 A C
 B C
@@ -56,13 +56,13 @@ B C
 1 3
 2 3" "${result}"
 
-	result=`echo A B C D | ./comb.egi 100`
+	result=`echo A B C D | egison ./comb.egi -- 100`
 	assertEquals "A B C D" "${result}"
 
 }
 
 test_conv() {
-	result=`seq 10 | xargs | ./conv.egi`
+	result=`seq 10 | xargs | egison ./conv.egi`
 	assertEquals "1
 2
 3
@@ -74,10 +74,10 @@ test_conv() {
 9
 10" "${result}"
 
-	result=`seq 10 | ./conv.egi eor=@`
+	result=`seq 10 | egison ./conv.egi -- eor=@`
 	assertEquals "1@2@3@4@5@6@7@8@9@10@" "${result}"
 
-	result=`seq 10 | xargs -n 5 | ./conv.egi each`
+	result=`seq 10 | xargs -n 5 | egison ./conv.egi -- each`
 	assertEquals "1
 2
 3
@@ -89,7 +89,7 @@ test_conv() {
 9
 10" "${result}"
 
-	result=`seq 10 | ./conv.egi 2`
+	result=`seq 10 | egison ./conv.egi -- 2`
 	assertEquals "1 2
 2 3
 3 4
@@ -100,7 +100,7 @@ test_conv() {
 8 9
 9 10" "${result}"
 
-	result=`printf "AA AB AC AD\nBA BB BC BD" | ./conv.egi 3`
+	result=`printf "AA AB AC AD\nBA BB BC BD" | egison ./conv.egi -- 3`
 	assertEquals "AA AB AC
 AB AC AD
 AC AD BA
@@ -108,7 +108,7 @@ AD BA BB
 BA BB BC
 BB BC BD" "${result}"
 
-# 	result=`printf "AA AB AC AD\nBA BB BC BD" | ./conv.egi ofs='"' 3`
+# 	result=`printf "AA AB AC AD\nBA BB BC BD" | egison ./conv.egi -- ofs='"' 3`
 # 	assertEquals 'AA"AB"AC
 # AB"AC"AD
 # AC"AD"BA
@@ -116,7 +116,7 @@ BB BC BD" "${result}"
 # BA"BB"BC
 # BB"BC"BD' "${result}"
 
-# 	result=`echo 'AA"AB"AC"AD"BA"BB"BC"BD' | ./conv.egi ifs="\"" ofs="\"\"" 3`
+# 	result=`echo 'AA"AB"AC"AD"BA"BB"BC"BD' | egison ./conv.egi -- ifs="\"" ofs="\"\"" 3`
 # 	assertEquals 'AA""AB""AC
 # AB""AC""AD
 # AC""AD""BA
@@ -124,13 +124,13 @@ BB BC BD" "${result}"
 # BA""BB""BC
 # BB""BC""BD' "${result}"
 
-	result=`printf "AA AB AC AD\nBA BB BC BD" | ./conv.egi each 3`
+	result=`printf "AA AB AC AD\nBA BB BC BD" | egison ./conv.egi -- each 3`
 	assertEquals "AA AB AC
 AB AC AD
 BA BB BC
 BB BC BD" "${result}"
 
-	result=`printf "AA AB AC AD\nBA BB BC BD" | ./conv.egi each eos="@@@" 3`
+	result=`printf "AA AB AC AD\nBA BB BC BD" | egison ./conv.egi -- each eos="@@@" 3`
 	assertEquals "AA AB AC
 AB AC AD
 @@@
@@ -139,49 +139,49 @@ BB BC BD" "${result}"
 }
 
 test_flat() {
-	result=`seq 10 | ./flat.egi`
+	result=`seq 10 | egison ./flat.egi`
 	assertEquals "1 2 3 4 5 6 7 8 9 10" "${result}"
 
-	result=`seq 10 | tr ' ' '@' | ./flat.egi fs=@`
+	result=`seq 10 | tr ' ' '@' | egison ./flat.egi -- fs=@`
 	assertEquals "1@2@3@4@5@6@7@8@9@10" "${result}"
 
-	result=`seq 10 | ./flat.egi 5 | ./flat.egi each`
+	result=`seq 10 | egison ./flat.egi -- 5 | ./flat.egi each`
 	assertEquals "1 2 3 4 5
 6 7 8 9 10" "${result}"
 
-	result=`seq 10 | ./flat.egi 2`
+	result=`seq 10 | egison ./flat.egi -- 2`
 	assertEquals "1 2
 3 4
 5 6
 7 8
 9 10" "${result}"
 
-# 	result=`seq 10 | ./flat.egi ofs='"\' 2`
+# 	result=`seq 10 | egison ./flat.egi -- ofs='"\' 2`
 # 	assertEquals '1"\2
 # 3"\4
 # 5"\6
 # 7"\8
 # 9"\10' "${result}"
 
-	result=`printf "AA AB AC AD\nBA BB BC BD" | ./flat.egi 3`
+	result=`printf "AA AB AC AD\nBA BB BC BD" | egison ./flat.egi -- 3`
 	assertEquals "AA AB AC
 AD BA BB
 BC BD" "${result}"
 
-	result=`printf "AA AB AC AD\nBA BB BC BD" | ./flat.egi each 3`
+	result=`printf "AA AB AC AD\nBA BB BC BD" | egison ./flat.egi -- each 3`
 	assertEquals "AA AB AC
 AD
 BA BB BC
 BD" "${result}"
 
-	result=`printf "AA AB AC AD\nBA BB BC BD" | ./flat.egi each eos="@@@" 3`
+	result=`printf "AA AB AC AD\nBA BB BC BD" | egison ./flat.egi -- each eos="@@@" 3`
 	assertEquals "AA AB AC
 AD
 @@@
 BA BB BC
 BD" "${result}"
 
-# 	result=`printf "AA AB AC AD\nBA BB BC BD" | ./flat.egi each eos='\' 3`
+# 	result=`printf "AA AB AC AD\nBA BB BC BD" | egison ./flat.egi -- each eos='\' 3`
 # 	assertEquals 'AA AB AC
 # AD
 # \
@@ -190,7 +190,7 @@ BD" "${result}"
 }
 
 test_slit() {
-	result=`seq 10 | ./slit.egi`
+	result=`seq 10 | egison ./slit.egi`
 	assertEquals "1
 2
 3
@@ -202,40 +202,40 @@ test_slit() {
 9
 10" "${result}"
 
-	result=`seq 10 | ./slit.egi 4`
+	result=`seq 10 | egison ./slit.egi -- 4`
 	assertEquals "1 2 3
 4 5 6
 7 8
 9 10" "${result}"
 
-	result=`seq 10 | ./slit.egi ofs="-" 5`
+	result=`seq 10 | egison ./slit.egi -- ofs="-" 5`
 	assertEquals "1-2
 3-4
 5-6
 7-8
 9-10" "${result}"
 
-	result=`seq 10 | ./flat.egi ofs="@" | ./slit.egi fs="@" 2`
+	result=`seq 10 | egison ./flat.egi -- ofs="@" | ./slit.egi fs="@" 2`
 	assertEquals "1@2@3@4@5
 6@7@8@9@10" "${result}"
 
-	result=`printf "AA AB AC AD\nBA BB BC BD" | ./slit.egi 3`
+	result=`printf "AA AB AC AD\nBA BB BC BD" | egison ./slit.egi -- 3`
 	assertEquals "AA AB AC
 AD BA BB
 BC BD" "${result}"
 
-	result=`printf "AA AB AC AD AE\nBA BB BC BD BE" | ./slit.egi each 2`
+	result=`printf "AA AB AC AD AE\nBA BB BC BD BE" | egison ./slit.egi -- each 2`
 	assertEquals "AA AB AC
 AD AE
 BA BB BC
 BD BE" "${result}"
 
-	result=`echo "AA AB AC AD" | ./slit.egi 3`
+	result=`echo "AA AB AC AD" | egison ./slit.egi -- 3`
 	assertEquals "AA AB
 AC
 AD" "${result}"
 
-	result=`printf "AA AB AC AD AE\nBA BB BC BD BE" | ./slit.egi each 4`
+	result=`printf "AA AB AC AD AE\nBA BB BC BD BE" | egison ./slit.egi -- each 4`
 	assertEquals "AA AB
 AC
 AD
@@ -245,7 +245,7 @@ BC
 BD
 BE" "${result}"
 
-	result=`printf "AA AB AC AD\nBA BB BC BD" | ./slit.egi each eos="@@@" 3`
+	result=`printf "AA AB AC AD\nBA BB BC BD" | egison ./slit.egi -- each eos="@@@" 3`
 	assertEquals "AA AB
 AC
 AD
@@ -257,7 +257,7 @@ BD" "${result}"
 }
 
 test_crops() {
-	result=`echo "(* (cos α) (cos β))" | ./crops.egi "\(.*\)"`
+	result=`echo "(* (cos α) (cos β))" | egison ./crops.egi -- "\(.*\)"`
 	assertEquals "(* (cos α)
 (cos α)
 (* (cos α) (cos β)
@@ -269,7 +269,7 @@ test_crops() {
 }
 
 test_cycle() {
-	result=`echo A B C D E | ./cycle.egi`
+	result=`echo A B C D E | egison ./cycle.egi`
 	assertEquals "A B C D E
 B C D E A
 C D E A B
@@ -278,28 +278,28 @@ E A B C D" "${result}"
 }
 
 test_dropl() {
-	result=`echo A B C D E F G | ./dropl.egi 3`
+	result=`echo A B C D E F G | egison ./dropl.egi -- 3`
 	assertEquals "D E F G" "${result}"
 
-	result=`echo A B C D E F G | ./dropl.egi ofs=_ 3`
+	result=`echo A B C D E F G | egison ./dropl.egi -- ofs=_ 3`
 	assertEquals "D_E_F_G" "${result}"
 }
 
 test_dropr() {
-	result=`echo A B C D E F G | ./dropr.egi 3`
+	result=`echo A B C D E F G | egison ./dropr.egi -- 3`
 	assertEquals "A B C D" "${result}"
 
-	result=`echo A B C D E F G | ./dropr.egi ofs=_ 3`
+	result=`echo A B C D E F G | egison ./dropr.egi -- ofs=_ 3`
 	assertEquals "A_B_C_D" "${result}"
 }
 
 test_dupl() {
-	result=`echo A B C D E F G | ./dupl.egi 3`
+	result=`echo A B C D E F G | egison ./dupl.egi -- 3`
 	assertEquals "A B C D E F G
 A B C D E F G
 A B C D E F G" "${result}"
 
-	result=`printf "A B C\nD E F G" | ./dupl.egi ofs=_ eos=--- 3`
+	result=`printf "A B C\nD E F G" | egison ./dupl.egi -- ofs=_ eos=--- 3`
 	assertEquals "A_B_C
 A_B_C
 A_B_C
@@ -310,16 +310,16 @@ D_E_F_G" "${result}"
 }
 
 test_mirror() {
-	result=`echo AAA BBB CCC AAA | ./mirror.egi`
+	result=`echo AAA BBB CCC AAA | egison ./mirror.egi`
 	assertEquals "AAA CCC BBB AAA" "${result}"
 }
 
 test_obrev() {
-	result=`echo AAA BBB CCC AAA | ./obrev.egi`
+	result=`echo AAA BBB CCC AAA | egison ./obrev.egi`
 	assertEquals "AAA BBB CCC AAA
 AAA CCC BBB AAA" "${result}"
 
-	result=`printf "1 2 3 4\nA B C D" | ./obrev.egi eos=---`
+	result=`printf "1 2 3 4\nA B C D" | egison ./obrev.egi -- eos=---`
 	assertEquals "1 2 3 4
 4 3 2 1
 ---
@@ -329,17 +329,17 @@ D C B A" "${result}"
 }
 
 test_nestl() {
-	result=`echo AAA BBB CCC | ./nestl.egi "<p>*</p>"`
+	result=`echo AAA BBB CCC | egison ./nestl.egi -- "<p>*</p>"`
 	assertEquals "<p> <p> <p> AAA </p> BBB </p> CCC </p>" "${result}"
 }
 
 test_nestr() {
-	result=`echo AAA BBB CCC | ./nestr.egi "<p>*</p>"`
+	result=`echo AAA BBB CCC | egison ./nestr.egi -- "<p>*</p>"`
 	assertEquals "<p> AAA <p> BBB <p> CCC </p> </p> </p>" "${result}"
 }
 
 test_perm() {
-	result=`echo A B C D | ./perm.egi 2`
+	result=`echo A B C D | egison ./perm.egi -- 2`
 	assertEquals "A B
 A C
 B A
@@ -353,7 +353,7 @@ C D
 D B
 D C" "${result}"
 
-	result=`echo A B C D | ./perm.egi 100`
+	result=`echo A B C D | egison ./perm.egi -- 100`
 	assertEquals "A B C D
 A B D C
 A C B D
@@ -381,7 +381,7 @@ D C B A" "${result}"
 }
 
 test_stairl() {
-	result=`echo A B C D | ./stairl.egi`
+	result=`echo A B C D | egison ./stairl.egi`
 	assertEquals "A
 A B
 A B C
@@ -389,7 +389,7 @@ A B C D" "${result}"
 }
 
 test_stairr() {
-	result=`echo A B C D | ./stairr.egi`
+	result=`echo A B C D | egison ./stairr.egi`
 	assertEquals "D
 C D
 B C D
@@ -397,7 +397,7 @@ A B C D" "${result}"
 }
 
 test_sublist() {
-	result=`echo A B C D | ./sublist.egi`
+	result=`echo A B C D | egison ./sublist.egi`
 	assertEquals "A
 A B
 B
@@ -411,7 +411,7 @@ D" "${result}"
 }
 
 test_subset() {
-	result=`echo A B C D | ./subset.egi`
+	result=`echo A B C D | egison ./subset.egi`
 	assertEquals "A
 B
 C
@@ -431,70 +431,70 @@ A B C D" "${result}"
 
 
 test_takel() {
-	result=`echo A B C D E F G | ./takel.egi 3`
+	result=`echo A B C D E F G | egison ./takel.egi -- 3`
 	assertEquals "A B C" "${result}"
 
-	result=`echo A B C D E F G | ./takel.egi 100`
+	result=`echo A B C D E F G | egison ./takel.egi -- 100`
 	assertEquals "A B C D E F G" "${result}"
 
-	result=`echo A B C D E F G | ./takel.egi ofs=_ 3`
+	result=`echo A B C D E F G | egison ./takel.egi -- ofs=_ 3`
 	assertEquals "A_B_C" "${result}"
 }
 
 test_taker() {
-	result=`echo A B C D E F G | ./taker.egi 3`
+	result=`echo A B C D E F G | egison ./taker.egi -- 3`
 	assertEquals "E F G" "${result}"
 
-	result=`echo A B C D E F G | ./taker.egi 100`
+	result=`echo A B C D E F G | egison ./taker.egi -- 100`
 	assertEquals "A B C D E F G" "${result}"
 
-	result=`echo A B C D E F G | ./taker.egi ofs=_ 3`
+	result=`echo A B C D E F G | egison ./taker.egi -- ofs=_ 3`
 	assertEquals "E_F_G" "${result}"
 }
 
 test_takelx() {
-	result=`echo QBY JCG FCM PAG TPX BQG UGB | ./takelx.egi "^P.*$"`
+	result=`echo QBY JCG FCM PAG TPX BQG UGB | egison ./takelx.egi -- "^P.*$"`
     assertEquals "QBY JCG FCM PAG" "${result}"
 
-	result=`echo QBY JCG FCM PAG TPX BQG UGB | ./takelx.egi ofs=_ "^P.*$"`
+	result=`echo QBY JCG FCM PAG TPX BQG UGB | egison ./takelx.egi -- ofs=_ "^P.*$"`
     assertEquals "QBY_JCG_FCM_PAG" "${result}"
 }
 
 test_takerx() {
-	result=`echo QBY JCG FCM PAG TPX BQG UGB | ./takerx.egi "^P.*$"`
+	result=`echo QBY JCG FCM PAG TPX BQG UGB | egison ./takerx.egi -- "^P.*$"`
     assertEquals "PAG TPX BQG UGB" "${result}"
 
-	result=`echo QBY JCG FCM PAG TPX BQG UGB | ./takerx.egi fs=" " ofs="*" "^P.*$"`
+	result=`echo QBY JCG FCM PAG TPX BQG UGB | egison ./takerx.egi -- fs=" " ofs="*" "^P.*$"`
     assertEquals "PAG*TPX*BQG*UGB" "${result}"
 }
 
 test_wrap() {
-	result=`echo aaa bbb ccc | ./wrap.egi "<p>*</p>"`
+	result=`echo aaa bbb ccc | egison ./wrap.egi -- "<p>*</p>"`
     assertEquals "<p>aaa</p> <p>bbb</p> <p>ccc</p>" "${result}"
 
-	result=`echo aaa bbb ccc | ./wrap.egi ofs=_ "<p>*</p>"`
+	result=`echo aaa bbb ccc | egison ./wrap.egi -- ofs=_ "<p>*</p>"`
     assertEquals "<p>aaa</p>_<p>bbb</p>_<p>ccc</p>" "${result}"
 }
 
 test_zniq() {
-	result=`echo aaa bbb ccc aaa bbb | ./zniq.egi`
+	result=`echo aaa bbb ccc aaa bbb | egison ./zniq.egi`
     assertEquals "aaa bbb ccc" "${result}"
 
-	result=`echo aaa bbb ccc aaa bbb | ./zniq.egi ofs="*"`
+	result=`echo aaa bbb ccc aaa bbb | egison ./zniq.egi -- ofs="*"`
     assertEquals "aaa*bbb*ccc" "${result}"
 }
 
 test_zrep() {
-	result=`echo 1 2 3 4 5 6 7 8 9 10 | ./zrep.egi "1"`
+	result=`echo 1 2 3 4 5 6 7 8 9 10 | egison ./zrep.egi -- "1"`
     assertEquals "1 10" "${result}"
 
-	result=`echo 1 2 3 4 5 6 7 8 9 10 | ./zrep.egi 1`
+	result=`echo 1 2 3 4 5 6 7 8 9 10 | egison ./zrep.egi -- 1`
     assertEquals "1 10" "${result}"
 
-	result=`echo AD1 AD2 AF1 AF2 BD1 BD2 BF1 BF2 CD1 CD2 CF1 CF2 DD1 DD2 DF1 DF2 | ./zrep.egi '^D.*$'`
+	result=`echo AD1 AD2 AF1 AF2 BD1 BD2 BF1 BF2 CD1 CD2 CF1 CF2 DD1 DD2 DF1 DF2 | egison ./zrep.egi -- '^D.*$'`
     assertEquals "DD1 DD2 DF1 DF2" "${result}"
 
-	result=`echo AD1 AD2 AF1 AF2 BD1 BD2 BF1 BF2 CD1 CD2 CF1 CF2 DD1 DD2 DF1 DF2 | ./zrep.egi 'あ'`
+	result=`echo AD1 AD2 AF1 AF2 BD1 BD2 BF1 BF2 CD1 CD2 CF1 CF2 DD1 DD2 DF1 DF2 | egison ./zrep.egi -- 'あ'`
     assertEquals "" "${result}"
 }
 
