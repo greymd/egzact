1e13d2c020a26c0233f5684a5f74488b41633e9e
greymd <yamadagrep@gmail.com>
2020-04-19 13:16:54 +0100

    Update packages to v2.0.0

--- package.sh
+++ package.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
 set -ue
 readonly THIS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]:-${(%):-%N}}")" && pwd)"
+trap 'rm -f "${THIS_DIR}"/egzact.tar.gz' EXIT
 {
   cd "${THIS_DIR}"
   rm -f "${THIS_DIR}"/egzact.tar.gz
-  mkdir -p "${THIS_DIR}/pkg/"{bin,lib}
-  (
-  cd "${THIS_DIR}/bin"
-  for f in *.egi; do
-    sed '/load-file/s|../lib|/usr/lib|' < "$f" > "${THIS_DIR}/pkg/bin/${f%.egi}"
-  done
-  )
-  cp -rf "${THIS_DIR}"/lib/egzact "${THIS_DIR}"/pkg/lib/
-  _version="$(awk '/^version/{print $NF}' "${THIS_DIR}"/pkg/.tar2package.yml)"
-  tar zcvf egzact.tar.gz -C "${THIS_DIR}/pkg" bin lib .tar2package.yml \
-    && docker run -i greymd/tar2rpm < egzact.tar.gz > "${THIS_DIR}"/pkg/egzact-"${_version}".rpm \
-    && docker run -i greymd/tar2deb < egzact.tar.gz > "${THIS_DIR}"/pkg/egzact-"${_version}".deb
-  rm -f "${THIS_DIR}"/egzact.tar.gz
-  rm -rf "${THIS_DIR}"/pkg/{bin,lib}
+  docker run -v "${PWD}":/work -i phusion/baseimage:latest /bin/bash -c '
+    _pholder="@@@@@REPLACE_HERE@@@@@"
+    /work/install.sh /usr/$_pholder
+    shopt -s globstar
+    sed -i "s|/$_pholder||g" /usr/$_pholder/{bin/*,lib/**/*.egi}
+    cp /work/pkg/.tar2package.yml /usr/$_pholder/
+    tar zcvf /work/egzact.tar.gz -C "/usr/$_pholder" bin lib .tar2package.yml
+  '
+  _version="$(awk "/^version/{print \$NF}" "${THIS_DIR}"/pkg/.tar2package.yml)"
+  docker run -i greymd/tar2rpm < egzact.tar.gz > "${THIS_DIR}"/pkg/egzact-"${_version}".rpm
+  docker run -i greymd/tar2deb < egzact.tar.gz > "${THIS_DIR}"/pkg/egzact-"${_version}".deb
 }
